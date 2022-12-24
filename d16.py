1be280b43167d5683146f86f10be2208296665a3
Kent Hou Man <knthmn0@gmail.com>
2022-12-24 22:37:35 +1100

    Finish day 16 part 2
    The trick here is to pull `acc` out of a recursion so the function can be memorized effectively. Part 2 runs about 2 min on my machine, there should be further optimization possible.

--- d16.py
+++ d16.py
@@ -1,3 +1,4 @@
+import functools
 import re
 from collections import deque
 from typing import NamedTuple
@@ -21,66 +22,61 @@ def parse(lines: list[str]) -> dict[str, Valve]:
     return valves
 
 
-def part1(lines: list[str]):
+def calculate_distance_map(origin, valves):
+    distance_map = {origin: 0}
+    paths = deque([(origin, 0)])
+
+    while paths and len(distance_map) != len(valves):
+        current_name, distance = paths.popleft()
+        valve = valves[current_name]
+        for dest in valve.tunnels:
+            if dest in distance_map:
+                continue
+            distance_map[dest] = distance + 1
+            paths.append((dest, distance + 1))
+    return distance_map
+
+
+def find_maximum_pressure(lines: list[str], initial_time_remaining=30, num_agents=1):
     valves = parse(lines)
     good_valves = set(name for name, pipe in valves.items() if pipe.flow_rate != 0)
+    distances = {
+        origin: calculate_distance_map(origin, valves)
+        for origin in {"AA"} | good_valves
+    }
 
-    def generate_distances(origin):
-        distances = {origin: 0}
-        paths = deque([(origin, 0)])
-
-        while paths and len(distances) != len(valves):
-            current_name, distance = paths.popleft()
-            valve = valves[current_name]
-            for dest in valve.tunnels:
-                if dest in distances:
+    @functools.cache
+    def recursive(agents: tuple[tuple[str, int], ...], turned: frozenset[str]) -> int:
+        max_value = 0
+        next_valves = good_valves - turned
+        for agent_index in range(len(agents)):
+            current, time_remaining = agents[agent_index]
+            _, flow_rate, tunnels = valves[current]
+            new_agents = list(agents)
+            for dest in next_valves:
+                if distances[current][dest] + 1 >= time_remaining:
                     continue
-                distances[dest] = distance + 1
-                paths.append((dest, distance + 1))
-        return {
-            dest: distance
-            for dest, distance in distances.items()
-            if dest in good_valves and dest != origin
-        }
+                new_remaining_time = time_remaining - distances[current][dest] - 1
+                new_agents[agent_index] = (dest, new_remaining_time)
+                dest_gain = valves[dest].flow_rate * new_remaining_time
+                further_gain = recursive(
+                    tuple(sorted(new_agents)), frozenset(turned | {dest})
+                )
+                max_value = max(max_value, dest_gain + further_gain)
+        return max_value
 
-    distances = {origin: generate_distances(origin) for origin in {"AA"} | good_valves}
-
-    def find_max_pressure(
-        current: str, remaining_time: int, acc: int, turned: set[str]
-    ) -> int:
-        if remaining_time <= 0 or len(turned) == len(good_valves):
-            return acc
-        _, flow_rate, tunnels = valves[current]
-        next_valves = good_valves - turned - {current}
-        max_pressure = max(
-            find_max_pressure(
-                dest,
-                remaining_time - distances[current][dest] - 1,
-                acc
-                + max(0, remaining_time - distances[current][dest] - 1)
-                * valves[dest].flow_rate,
-                turned | {dest},
-            )
-            for dest in next_valves
-        )
-        return max_pressure
-
-    return find_max_pressure("AA", 30, 0, set())
-
-
-def part2(lines: list[str]):
-    ...
+    return recursive((("AA", initial_time_remaining),) * num_agents, frozenset())
 
 
 def test():
     with open("d16-t.txt") as f:
         lines = f.readlines()
-    assert part1(lines) == 1651
-    assert part2(lines) == None
+    assert find_maximum_pressure(lines) == 1651
+    assert find_maximum_pressure(lines, 26, 2) == 1707
 
 
 if __name__ == "__main__":
     with open("input/16.txt") as f:
         lines = f.readlines()
-    print(part1(lines))
-    print(part2(lines))
+    print(find_maximum_pressure(lines))
+    print(find_maximum_pressure(lines, 26, 2))
